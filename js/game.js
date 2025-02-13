bdfaaaf950f88c9d61b4622f00c2849ba6c7ce07
RazSoft <razgroupgame@gmail.com>
2025-02-13 15:04:22 +0530

    Add default theam system and file

--- js/game.js
+++ js/game.js
@@ -14,15 +14,62 @@ class Game {
     // TODO: First method to run when game will start
     init() {
         this.hideScreens();
-        this.loadTheam('theam1');
-        this.showScreen('main-menu');
+        this.loadTheam('theam1').then(()=> {
+            this.showScreen('main-menu');
+        });
     }
     
     // TODO: Method to load the theam
-    loadTheam(theam){
+    async loadTheam(theam){
+        console.log('loading theam');
+        
+        let theamObj = theamList[theam];
+        
+        // Loading theam css to the document
+        const existingLink = document.querySelector('link[data-theam="true"]');
+        if(existingLink){
+            existingLink.parentNode.removeChild(existingLink);
+        } 
+        // Create new Link element
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = './js/theam/' + theamObj.folder + '/' + theamObj.css;
+        link.setAttribute('data-theam', 'true');
+        document.head.appendChild(link);
+        
+        let loadingCss = new Promise( function(resolve, reject){
+            link.onload = () => resolve('Css loaded')
+            link.onerror = () => reject('Error while loading css')
+        });
+
+        
+        console.log(await loadingCss);
+        console.log('loading theam in middle');
+
+        // Loading theam javascript file
+        const existingSrc = document.querySelector('script[data-theam="true"]');
+        if(existingSrc){
+            existingSrc.parentNode.removeChild(existingSrc);
+        } 
+        // Create new src element
+        const script = document.createElement('script');
+        script.src = './js/theam/' + theamObj.folder + '/' + theamObj.js;
+        script.setAttribute('data-theam', 'true');
+        document.body.appendChild(script);
+
+        let loadingJs = new Promise(function(resolve, reject){
+            script.onload = () => resolve('JS loaded');
+            script.onerror = () => resolve('Error while loading JS');
+        });
+
+        console.log(await loadingJs);
+
+        let theamImp = new TheamImp();
         let gameContainer = document.getElementById('game-container');
-        let gameHtml = theam.mainMenu();
+        let gameHtml = theamImp.mainMenu();
         gameContainer.innerHTML = gameHtml;
+        console.log('theam loaded successfylly');
+        return "Theam loaded"; 
     } 
 
     // TODO: Implement method to hide all the screen implement screen screen class. 
