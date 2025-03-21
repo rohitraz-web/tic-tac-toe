83aa83b48b35afb42fde515181635629f60d3023
RazSoft <razgroupgame@gmail.com>
2025-03-21 18:04:32 +0530

    Add resize game container code in socurce file

--- js/source.js
+++ js/source.js
@@ -4,26 +4,26 @@ console.log('Javascript file loadede');
 
 window.onload = function(event) {
     // Loading the game according to the screen size
-    // if(window.innerWidth <= 600 || window.innerHeight <= 700){
-    //     resizeGameContainer();
-    //     console.log('Container Resized');
-    // }
     let game = new Game();
+
+    resizeGameContainer();
 };
+
 window.addEventListener('resize', resizeGameContainer);
 
 // window.onresize(resizeGameContainer);
 // Method to resize the screen of the game
+
 function resizeGameContainer(event){
     // Scaling the game
-    // let scaleX = window.innerWidth/600;
-    // let scaleY = window.innerHeight/700;
-    // let scale = Math.min(scaleX, scaleY);
+    if((window.innerWidth <= 600 || window.innerHeight <= 700)){
+        
+        let scaleX = window.innerWidth/600;
+        let scaleY = window.innerHeight/700;
+        let scale = Math.min(scaleX, scaleY);
 
-    // let element = document.getElementById('game-container');
-    // let elements = element.getElementsByTagName('*');
-    // for (let i = 0; i < elements.length; i++) {
-    //     elements[i].style.transform = "scale("+scale+","+scale+")";
-    // }
-    // element.style.transform = "scale("+scale+","+scale+")";
+        let element = document.getElementById('game-container');
+        element.style.transform = "scale("+scale+","+scale+")";
+    
+    }
 }
