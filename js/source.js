9eedc6a251438e561133f9293ad5ae0e1098f3f0
RazSoft <razgroupgame@gmail.com>
2025-03-21 19:13:25 +0530

    Add resize method to the game board

--- js/source.js
+++ js/source.js
@@ -16,14 +16,17 @@ window.addEventListener('resize', resizeGameContainer);
 
 function resizeGameContainer(event){
     // Scaling the game
-    if((window.innerWidth <= 600 || window.innerHeight <= 700)){
+    if((window.innerWidth <= 500 || window.innerHeight <= 600)){
         
-        let scaleX = window.innerWidth/600;
-        let scaleY = window.innerHeight/700;
-        let scale = Math.min(scaleX, scaleY);
+    let scaleX = window.innerWidth/600;
+    let scaleY = window.innerHeight/700;
+    let scale = Math.min(scaleX, scaleY);
 
-        let element = document.getElementById('game-container');
-        element.style.transform = "scale("+scale+","+scale+")";
-    
+    let element = document.getElementById('game-container');
+    element.style.transformOrigin = "top left";
+    element.style.top = "50%";
+    element.style.left = "50%";
+    element.style.position = "absolute";
+    element.style.transform = "scale("+scale+","+scale+") translate(-50%, -50%)";
     }
 }
