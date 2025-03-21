d0332fd7a284071ceb6e2f7244ad5bb7b450f565
RazSoft <razgroupgame@gmail.com>
2025-03-21 17:57:16 +0530

    Add resize method to game container

--- js/source.js
+++ js/source.js
@@ -16,14 +16,10 @@ window.addEventListener('resize', resizeGameContainer);
 // Method to resize the screen of the game
 function resizeGameContainer(event){
     // Scaling the game
-    // let scaleX = window.innerWidth/600;
-    // let scaleY = window.innerHeight/700;
-    // let scale = Math.min(scaleX, scaleY);
+    let scaleX = window.innerWidth/500;
+    let scaleY = window.innerHeight/600;
+    let scale = Math.min(scaleX, scaleY);
 
-    // let element = document.getElementById('game-container');
-    // let elements = element.getElementsByTagName('*');
-    // for (let i = 0; i < elements.length; i++) {
-    //     elements[i].style.transform = "scale("+scale+","+scale+")";
-    // }
-    // element.style.transform = "scale("+scale+","+scale+")";
+    let element = document.getElementById('game-container');
+    element.style.transform = "scale("+scale+")";
 }
